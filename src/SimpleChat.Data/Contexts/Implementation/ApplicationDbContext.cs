05d0f095765d550905bfbe108fd23281149e693f
Kolya Voitekhovich <kolya.vaitihovich09@gmail.com>
2023-10-12 00:22:15 +0300

    Add db context

---
+++ src/SimpleChat.Data/Contexts/Implementation/ApplicationDbContext.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SimpleChat.Data.Contexts.Contracts;
+using SimpleChat.Data.Entities;
+using System.Reflection;
+
+namespace SimpleChat.Data.Contexts.Implementation
+{
+    public class ApplicationDbContext : DbContext, IApplicationDbContext
+    {
+        public DbSet<UserEntity> Users { get; set; } = null!;
+        public DbSet<ChatEntity> Chats { get; set; } = null!;
+        public DbSet<UserChatEntity> UserChats { get; set; } = null!;
+
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) { }
+
+        public async Task<int> SaveChangesAsync() => await base.SaveChangesAsync();
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+        }
+    }
+}
---
+++ src/SimpleChat.Data/IoC/ServicesExtensions.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using SimpleChat.Data.Contexts.Contracts;
+using SimpleChat.Data.Contexts.Implementation;
+
+namespace SimpleChat.Data.IoC
+{
+    public static class ServicesExtensions
+    {
+        public static IServiceCollection ConfigureDataLayer(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.ConfigureSqlContext(configuration)
+                .ConfigureDbContext();
+
+            return services;
+        }
+
+        public static IServiceCollection ConfigureSqlContext(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddDbContext<IApplicationDbContext, ApplicationDbContext>(options =>
+                options.UseSqlServer(configuration.GetConnectionString("sqlConnection")),
+                ServiceLifetime.Transient);
+
+            return services;
+        }
+
+        public static IServiceCollection ConfigureDbContext(this IServiceCollection services)
+        {
+            services.AddScoped<IApplicationDbContext, ApplicationDbContext>();
+
+            return services;
+        }
+    }
+}
