68dbc0bd50ff26190870b96ec45a7b4585bf0bbc
Kolya Voitekhovich <kolya.vaitihovich09@gmail.com>
2023-10-12 00:13:28 +0300

    Add configurations

---
+++ src/SimpleChat.Data/Configurations/UserConfiguration.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using SimpleChat.Data.Entities;
+
+namespace SimpleChat.Data.Configurations
+{
+    public class UserConfiguration : IEntityTypeConfiguration<UserEntity>
+    {
+        public void Configure(EntityTypeBuilder<UserEntity> builder)
+        {
+            builder.HasKey(u => u.Id);
+            builder.Property(u => u.Id)
+                .UseIdentityColumn(1, 1)
+                .ValueGeneratedOnAdd();
+
+            builder.Property(u => u.FirstName)
+                .HasMaxLength(100)
+                .IsRequired();
+
+            builder.Property(u => u.LastName)
+                .HasMaxLength(100)
+                .IsRequired();
+
+            builder.Property(u => u.UserName)
+                .HasMaxLength(100)
+                .IsRequired();
+
+            builder.Property(u => u.Email)
+                .HasMaxLength(100)
+                .IsRequired();
+        }
+    }
+}
