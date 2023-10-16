a439096a1a1084344e73d4a64421640983d91f4c
Kolya Voitekhovich <kolya.vaitihovich09@gmail.com>
2023-10-17 00:28:25 +0300

    Add mapping tests

---
+++ SimpleChat.UnitTests/MappingTests/UserChatMappingTests.cs
@@ -0,0 +1,22 @@
+﻿using AutoMapper;
+using SimpleChat.Business.MappingProfiles;
+
+namespace SimpleChat.UnitTests.MappingTests
+{
+    public class UserChatMappingTests
+    {
+        [Fact]
+        public void ValidateUserChatMappingProfile_ShouldReturnSuccessResult()
+        {
+            MapperConfiguration mapperConfig = new MapperConfiguration(
+           cfg =>
+           {
+               cfg.AddProfile(new UserChatMappingProfile());
+           });
+
+            IMapper mapper = new Mapper(mapperConfig);
+
+            mapper.ConfigurationProvider.AssertConfigurationIsValid();
+        }
+    }
+}
